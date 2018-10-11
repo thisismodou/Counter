80b854f0a64e94ad651186bf1bcce09780f6f638
Modou Lo <thisismodou@outlook.com>
2018-10-11 07:50:31 -0400

    Now the increase or decrease button is automatically disabled if minimum or maximum number is reached, respectively.

--- counter.js
+++ counter.js
@@ -2,12 +2,26 @@ let decreaseButton = document.getElementById('counterMinusButton');
 let increaseButton = document.getElementById('counterPlusButton');
 let input = document.getElementById('counterField');
 
+decreaseButton.disabled = true;
+
 function increase() {
     input.stepUp(1);
+    if (input.value >= 1) {
+        decreaseButton.disabled = false;
+    }
+    if (input.value >= 10) {
+        increaseButton.disabled = true;
+    }
 }
 
 function decrease() {
     input.stepDown(1);
+    if (input.value == 1) {
+        decreaseButton.disabled = true;
+    }
+    if (input.value <= 9) {
+        increaseButton.disabled = false;
+    }
 }
 
 function resetToMin() {
@@ -22,9 +36,26 @@ function resetToMax() {
     }
 }
 
+// Disable relative button on 'Keyup' if the Up and Down arrows are used to increase or decrease number
+function disableButtons() {
+    if (input.value == 1) {
+        decreaseButton.disabled = true;
+    }else {
+        decreaseButton.disabled = false;
+    }
+    if (input.value == 10) {
+        increaseButton.disabled = true;
+    }else {
+        increaseButton.disabled = false;
+    }
+}
+
 increaseButton.addEventListener('click', increase);
 decreaseButton.addEventListener('click', decrease);
 input.addEventListener('focusout', resetToMax);
 input.addEventListener('focusout', resetToMin);
+input.addEventListener('focusout', disableButtons);
+input.addEventListener('focusout', disableButtons);
+input.addEventListener('keyup', disableButtons);
 
-// My first official Github Project
+// My first official Github Project 😉
